1bcc7b21d73ffa577d96fbf10b73e6a757f1e5fb
Muhsin <muhsinoztufek@gmail.com>
2023-07-24 11:54:20 +0300

    Updated basket_sql and product_list

--- basket_sql.php
+++ basket_sql.php
@@ -3,13 +3,25 @@ require_once("db_connection.php");
 
 // Sanitize and validate user input
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_POST['customerID']) && isset($_POST['productID'])) {
+
+    if (isset($_POST['islem']) && $_POST['islem'] == 'clearAll') {
+        $basketsql = new BasketSQL($conn);
+        $customerID = $_POST['customerID'];
+        $basketsql->clearBasket($customerID);
+        $data = $basketsql->getProductDetails($customerID);
+        http_response_code(200); // Success
+        echo json_encode($data);
+    }
+
+
+    if (isset($_POST['customerID']) && isset($_POST['productID']) && isset($_POST['change'])) {
         $customerID = $_POST['customerID'];
         $productID = $_POST['productID'];
+        $change = $_POST['change'];
 
         try {
             $basketsql = new BasketSQL($conn);
-            $basketsql->updateBasket($customerID, $productID);
+            $basketsql->updateBasket($customerID, $productID, $change);
             $data = $basketsql->getProductDetails($customerID);
             http_response_code(200); // Success
             echo json_encode($data);
@@ -40,7 +52,7 @@ class BasketSQL
         $this->conn = $conn;
     }
 
-    public function updateBasket($customerID, $productID)
+    public function updateBasket($customerID, $productID, $change)
     {
         $price = $this->getProductPrice($productID);
 
@@ -50,28 +62,24 @@ class BasketSQL
         $stmt->bindParam(':product_id', $productID);
         $stmt->execute();
 
-        if ($stmt->rowCount() === 0) {
+        if ($stmt->rowCount() == 0) {
             // No row exists, insert new row
             $this->insertBasket($customerID, $productID, $price);
         } else {
             // Row exists, update quantity and total_price
-            $this->updateExistingBasketUpper($customerID, $productID, $price);
+            if ($change == 1) {
+                $this->updateExistingBasketUpper($customerID, $productID, $price);
+            } elseif ($change == -1) {
+                $this->updateExistingBasketLower($customerID, $productID, $price);
+            }
         }
     }
+
     // Gets upper of given product basketQuantity
     private function updateExistingBasketUpper($customerID, $productID, $price)
     {
-        $sql = "UPDATE basket SET basket_quantity = basket_quantity + 1, total_price = (basket_quantity) * :price WHERE customer_id = :customer_id AND product_id = :product_id";
-        $stmt = $this->conn->prepare($sql);
-        $stmt->bindParam(':customer_id', $customerID);
-        $stmt->bindParam(':product_id', $productID);
-        $stmt->bindParam(':price', $price);
-        $stmt->execute();
-    }
-    // Gets lower of given product basketQuantity
-    private function updateExistingBasketLower($customerID, $productID, $price)
-    {
-        $sql = "UPDATE basket SET basket_quantity = basket_quantity - 1, total_price = (basket_quantity) * :price WHERE customer_id = :customer_id AND product_id = :product_id";
+        $sql = "UPDATE basket SET basket_quantity = basket_quantity + 1, total_price = (basket_quantity) * :price  WHERE customer_id = :customer_id AND product_id = :product_id";
+
         $stmt = $this->conn->prepare($sql);
         $stmt->bindParam(':customer_id', $customerID);
         $stmt->bindParam(':product_id', $productID);
@@ -79,14 +87,46 @@ class BasketSQL
         $stmt->execute();
     }
 
-    // Deletes all basket
-    private function deleteBasket($customerID)
+    private function beforeUpdateStockCheckker($productID)
     {
-        $sql = "DELETE FROM basket WHERE customer_id = :customer_id ";
+        $sql = "SELECT quantity FROM products WHERE product_id = :product_id";
+
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindParam(':product_id', $productID);
+        $stmt->execute();
+    }
+    // Gets lower of given product basketQuantity
+    private function updateExistingBasketLower($customerID, $productID, $price)
+    {
+        $sql = "UPDATE basket SET basket_quantity = basket_quantity - 1, total_price = (basket_quantity) * :price  WHERE customer_id = :customer_id AND product_id = :product_id";
+
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindParam(':customer_id', $customerID);
+        $stmt->bindParam(':product_id', $productID);
+        $stmt->bindParam(':price', $price);
+        $stmt->execute();
+    }
+
+    public function totalPrice($customerID)
+    {
+        $totalPrice = 0;
+        $sql = "SELECT total_price FROM basket WHERE customer_id = :customer_id";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindParam(':customer_id', $customerID);
+        $result =  $stmt->execute();
+
+        $result['price'] += $totalPrice;
+
+        return $totalPrice;
+    }
+    public function clearBasket($customerID)
+    {
+        $sql = "DELETE FROM basket WHERE customer_id = :customer_id";
         $stmt = $this->conn->prepare($sql);
         $stmt->bindParam(':customer_id', $customerID);
         $stmt->execute();
     }
+
     // Gets products price
     // Using in the upper functions
     private function getProductPrice($productID)
@@ -117,7 +157,7 @@ class BasketSQL
     // Gets product details to return table 
     public function getProductDetails($customerID)
     {
-        $sql = "SELECT p.product_name, p.brand_name, b.basket_quantity, p.price, b.total_price 
+        $sql = "SELECT p.id, p.product_name, p.brand_name, b.basket_quantity, p.price, b.total_price 
             FROM products AS p
             INNER JOIN basket AS b ON p.id = b.product_id
             WHERE b.customer_id = :customer_id";
--- product_list.php
+++ product_list.php
@@ -4,6 +4,10 @@
 
 <head>
     <style>
+        /* styles.css */
+        /* styles.css */
+
+        /* General Styles */
         body {
             font-family: Arial, sans-serif;
             margin: 0;
@@ -19,6 +23,7 @@
             justify-content: space-between;
         }
 
+        /* Product List and Cart Containers */
         .product-list,
         .cart {
             width: 48%;
@@ -29,9 +34,11 @@
 
         .product-list h2,
         .cart h2 {
-            margin-top: 0;
+            margin: 0 0 15px;
+            font-size: 20px;
         }
 
+        /* Table Styles */
         table {
             width: 100%;
             border-collapse: collapse;
@@ -44,6 +51,7 @@
             border-bottom: 1px solid #ccc;
         }
 
+        /* Add To Cart Button */
         .add-to-cart {
             background-color: #4CAF50;
             color: white;
@@ -52,13 +60,83 @@
             cursor: pointer;
         }
 
-        .cart-heading {
-            margin-top: 30px;
+        .add-to-cart:hover {
+            background-color: #45a049;
         }
 
+        /* Cart Buttons */
+        .cart button {
+            margin-right: 5px;
+            padding: 5px 8px;
+            border: none;
+            color: white;
+            cursor: pointer;
+            font-size: 14px;
+            border-radius: 4px;
+        }
+
+        .cart button.minus-btn {
+            background-color: #f44336;
+        }
+
+        .cart button.plus-btn {
+            background-color: #2196F3;
+        }
+
+        /* Clear Cart Button */
+        .clear-cart-container {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+
+        .clear-cart-button {
+            background-color: #f44336;
+            padding: 8px 15px;
+            border: none;
+            color: white;
+            cursor: pointer;
+            font-size: 16px;
+            border-radius: 4px;
+        }
+
+        .clear-cart-button:hover {
+            background-color: #d32f2f;
+        }
+
+        /* Cart Heading */
+        .cart-heading {
+            margin: 0;
+        }
+
+        /* Center align empty cart message */
         .cart-empty td {
             text-align: center;
         }
+
+        /* Align clear cart button to the right */
+        .clear-cart {
+            text-align: right;
+            margin-bottom: 15px;
+        }
+
+        .total-cart-value {
+            text-align: center;
+            margin-top: 20px;
+        }
+
+        .total-cart-value h2 {
+            margin: 0;
+            font-size: 18px;
+        }
+
+        .total-cart-value #cartTotalValue {
+            font-weight: bold;
+            color: #2196F3;
+            /* You can choose any color you prefer */
+        }
+
+        /* Add more styles and media queries if needed */
     </style>
 </head>
 
@@ -69,7 +147,7 @@
     </div>
     <div class="container">
         <div class="product-list">
-            <h2>Ürünler</h2>
+            <h2 class="cart-heading">Ürünler</h2>
             <table>
                 <thead>
                     <tr>
@@ -93,14 +171,21 @@
                             <td><?php echo $product['brand_name'] ?></td>
                             <td><?php echo $product['quantity'] ?></td>
                             <td><?php echo $product['price'] ?></td>
-                            <td><button class="btn add-to-cart" data-id="<?php echo  $product['id']; ?>">Sepete Ekle</button></td>
+                            <td>
+                                <button onclick="changeQuantity(<?php echo $product['id']; ?>, 1)" data-id="<?php echo $product['id']; ?>">Sepete Ekle</button>
+                            </td>
+
                         </tr>
                     <?php endforeach; ?>
                 </tbody>
             </table>
         </div>
         <div class="cart">
-            <h2 class="cart-heading">Sepetim</h2>
+            <div class="clear-cart-container">
+                <h2 class="cart-heading">Sepetim</h2>
+                <button class="clear-cart-button" onclick="clearAll()" id="clearAll">Clear Cart</button>
+            </div>
+
             <table>
                 <thead>
                     <tr>
@@ -115,87 +200,118 @@
                 </thead>
                 <tbody id="tbody"></tbody>
             </table>
+
+            <div class="total-cart-value">
+                <h2>Toplam Sepet Değeri: <span id="cartTotalValue">0</span></h2>
+            </div>
+
         </div>
-    </div>
-    <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
-    <script>
-        // Gets basket products while system loading 
-        $(document).ready(function() {
+        
+        <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
 
-            var customerID = $('#customer_id').data('id');
-            $.ajax({
-                url: "http://localhost/website/basket_sql.php",
-                data: {
-                    customerID: customerID
-                }, // Sending the customerID as an object
-                type: "POST",
-                success: function(result) {
-                    var products = JSON.parse(result);
-                    var table;
+        <script>
+            // Gets basket products while system loading 
+            $(document).ready(function() {
 
-                    for (var i = 0; i < products.length; i++) {
-                        var product = products[i];
-                        table += "<tr>";
-                        table += "<td>" + product.product_name + "</td>";
-                        table += "<td>" + product.brand_name + "</td>";
-                        table += "<td>" + product.basket_quantity + "</td>";
-                        table += "<td>" + product.price + "</td>";
-                        table += "<td>" + product.total_price + "</td>";
-                        table += "<td>" + product.total_price + "</td>"; //Eksiltme işlemi gelecek 
-                        table += "</tr>";
-                    }
-                    $('#tbody').append(table);
-                },
+                var customerID = $('#customer_id').data('id');
+                $.ajax({
+                    url: "http://localhost/website/basket_sql.php",
+                    data: {
+                        customerID: customerID
+                    }, // Sending the customerID as an object
+                    type: "POST",
+                    success: function(result) {
+                        var products = JSON.parse(result);
+                        var table;
 
+                        for (var i = 0; i < products.length; i++) {
+                            var product = products[i];
+                            table += "<tr>";
+                            table += "<td>" + product.product_name + "</td>";
+                            table += "<td>" + product.brand_name + "</td>";
+                            table += "<td>" + product.basket_quantity + "</td>";
+                            table += "<td>" + product.price + "</td>";
+                            table += "<td>" + products[i].total_price + "</td>";
+                            table += "<td>" + "<button onclick='changeQuantity(" + products[i].id + ", -1)'>-</button>";
+                            table += "<button onclick='changeQuantity(" + products[i].id + ", 1)'>+</button>" + "</td>";
+                            table += "</tr>";
+                        }
+                        $('#tbody').append(table);
+                    },
+
+                });
             });
-        });
-    </script>
-    <script>
-        $(document).ready(function() {
-            $('.add-to-cart').on('click', function() {
-                var productID = $(this).data('id');
+        </script>
+
+
+        <script>
+            function changeQuantity(productID, change) {
                 var customerID = $('#customer_id').data('id');
                 var data = {
                     productID: productID,
-                    customerID: customerID
+                    customerID: customerID,
+                    change: change
                 };
                 $.ajax({
                     url: "http://localhost/website/basket_sql.php",
                     data: data,
                     type: "POST",
-                    success: function(result2) {
+                    success: function(result) {
+                        var products = JSON.parse(result);
 
-                        var products = JSON.parse(result2);
-
-                        // Solution of undefined mistake
                         var table1 = "";
 
                         for (var i = 0; i < products.length; i++) {
-                            {
-                                table1 += "<tr>";
-                                table1 += "<td>" + products[i].product_name + "</td>";
-                                table1 += "<td>" + products[i].brand_name + "</td>";
-                                table1 += "<td>" + products[i].basket_quantity + "</td>";
-                                table1 += "<td>" + products[i].price + "</td>";
-                                table1 += "<td>" + products[i].total_price + "</td>";
-                                table1 += "<td>" + products[i].total_price + "</td>"; // Eksiltme işlemi gelecek.
-                                table1 += "</tr>";
-                            };
-
+                            table1 += "<tr>";
+                            table1 += "<td>" + products[i].product_name + "</td>";
+                            table1 += "<td>" + products[i].brand_name + "</td>";
+                            table1 += "<td>" + products[i].basket_quantity + "</td>";
+                            table1 += "<td>" + products[i].price + "</td>";
+                            table1 += "<td>" + products[i].total_price + "</td>";
+                            table1 += "<td>" + "<button onclick='changeQuantity(" + products[i].id + ", -1)'>-</button>";
+                            table1 += "<button onclick='changeQuantity(" + products[i].id + ", 1)'>+</button>" + "</td>";
+                            table1 += "</tr>";
                         }
 
                         var tbodyElement = document.getElementById("tbody");
 
-                        tbodyElement.innerHTML = "";
-
                         tbodyElement.innerHTML = table1;
-
                     }
-
                 });
-            });
-        });
-    </script>
+            }
+        </script>
+
+        <!-- Belongs to Clear Basket button   -->
+        <script>
+            function clearAll() {
+                $("#clearAll").click(function() {
+                    var customerID = $('#customer_id').data('id');
+                    var islem = "clearAll";
+                    var data = {
+                        customerID: customerID,
+                        islem: islem
+                    };
+
+                    $.ajax({
+                        url: "http://localhost/website/basket_sql.php",
+                        data: data,
+                        type: "POST",
+                        success: function(result) {
+                            var tbodyElement = document.getElementById("tbody");
+                            tbodyElement.innerHTML = "";
+                        },
+                        error: function(xhr, status, error) {
+                            // Handle error here if the AJAX request fails
+                            console.error(error);
+                        }
+                    });
+                });
+            }
+        </script>
+
+
+
+
 
 
 
