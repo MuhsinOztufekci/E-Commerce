f8d7bb7675f9c60e5e92c4b78bf33458e0f52541
Muhsin <muhsinoztufek@gmail.com>
2023-07-26 09:53:17 +0300

    Added warehouse part to header directions

--- include/header.php
+++ include/header.php
@@ -4,6 +4,7 @@
         <ul>
             <li><a href="dashboard.php?sayfa=anasayfa">Ana Sayfa</a></li>
             <li><a href="dashboard.php?sayfa=menu">Menu</a></li>
+            <li><a href="dashboard.php?sayfa=depolar">Depolar</a></li>
             <li><a href="dashboard.php?sayfa=sepet">Sepet</a></li>
             <li><a href="#">Hesabım</a></li>
             <li><a href="dashboard.php?sayfa=logout">Çıkış Yap</a></li>
