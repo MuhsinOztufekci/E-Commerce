c00f31e176c1902faedd967ac4bf018d4798fdc9
Muhsin <muhsinoztufek@gmail.com>
2023-07-31 08:56:02 +0300

    chechout page updated following SOLID principles

--- checkout_sql.php
+++ checkout_sql.php
@@ -2,72 +2,52 @@
 session_start();
 require('db_connection.php');
 
-// Check if the request is a POST request
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $customerID = $_POST['customerID']; // We get customerID from session
-    $grandTotal = 0; // Calculating grand total
-
-    // Create an instance of the FinishedPayment class
-    $finishedPayment = new FinishedPayment($conn);
-
-    // Get products from the basket for the customer
-    $products = $finishedPayment->getProductsFromBasket($customerID);
-
-    // Check if there are products in the basket
-    if (empty($products)) {
-        http_response_code(400); // Bad Request
-        echo "Basket is empty. Payment cannot be processed.";
-        exit;
-    }
-
-    // Iterate through the products and calculate the grand total
-    foreach ($products as $product) {
-        $grandTotal += $product["total_price"];
-    }
-
-    // Add order to the order table
-    $finishedPayment->addOrderTable($customerID, $grandTotal);
-
-    // Get the order ID for the newly inserted order
-    $orderID = $conn->lastInsertId();
-
-    // Add order details for each product to the order_details table
-    foreach ($products as $product) {
-        $productID = $product["id"];
-        $quantity = $product["basket_quantity"];
-        $finishedPayment->addOrderDetailsTable($orderID, $productID, $quantity);
-
-        $finishedPayment->lowerStockAmount($productID, $quantity);
-    }
-
-
-    // Clear the user's basket after completing the payment process
-    $cleared = $finishedPayment->clearBasket($customerID);
-
-    if ($cleared) {
-        // Send a response back to the JavaScript function
-        echo "Payment successful! Basket cleared.";
-    } else {
-        // Return an error response if the basket clearance failed
-        http_response_code(500); // Internal Server Error
-        echo "Payment successful, but basket clearance failed.";
-    }
-} else {
-    // Return an error response if the request method is not POST
-    http_response_code(405);
-    echo "Invalid request method.";
+interface PaymentGatewayInterface
+{
+    public function processPayment($customerID, &$errorMessage = "");
 }
 
-class FinishedPayment
+class PaymentManager
 {
     private $conn;
+    private $paymentGateway;
 
     public function __construct($conn)
     {
         $this->conn = $conn;
+        $this->paymentGateway = new YourPaymentGateway(); // Replace with the actual payment gateway implementation
     }
 
-    public function getProductsFromBasket($customerID)
+    public function processPayment($customerID)
+    {
+        // Basketten ürünleri aldık.
+        $products = $this->getProductsFromBasket($customerID);
+
+        if (empty($products)) {
+            http_response_code(400); // Bad Request
+            echo "Basket is empty. Payment cannot be processed.";
+            exit;
+        }
+
+        $grandTotal = 0;
+        foreach ($products as $product) {
+            $grandTotal += $product["total_price"];
+        }
+
+        $orderID = $this->addOrder($customerID, $grandTotal);
+
+        foreach ($products as $product) {
+            $productID = $product["id"];
+            $quantity = $product["basket_quantity"];
+            $this->addOrderDetails($orderID, $productID, $quantity);
+            $this->lowerStockAmount($productID, $quantity);
+        }
+
+        $cleared = $this->clearBasket($customerID);
+
+        return $cleared && $this->paymentGateway->processPayment($customerID);
+    }
+    private function getProductsFromBasket($customerID)
     {
         $sql = "SELECT p.id, p.product_name, p.brand_name, b.basket_quantity, p.price, b.total_price
                 FROM products AS p
@@ -81,35 +61,41 @@ class FinishedPayment
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public function addOrderTable($customerID, $grandTotal)
+    private function addOrder($customerID, $grandTotal)
     {
         $sql = "INSERT INTO `order` (customer_id, grand_total) VALUES (:customer_id, :grand_total)";
         $stmt = $this->conn->prepare($sql);
         $stmt->bindParam(':customer_id', $customerID);
         $stmt->bindParam(':grand_total', $grandTotal);
         $stmt->execute();
+
+        return $this->conn->lastInsertId();
     }
 
-    public function addOrderDetailsTable($orderID, $productID, $quantity)
+    private function addOrderDetails($orderID, $productID, $quantity)
     {
-        $sql = "INSERT INTO order_details (order_id, product_id, quantity) VALUES (:order_id, :product_id, :quantity)";
-        $stmt = $this->conn->prepare($sql);
-        $stmt->bindParam(':order_id', $orderID);
-        $stmt->bindParam(':product_id', $productID);
-        $stmt->bindParam(':quantity', $quantity);
-        $stmt->execute();
+        if (is_array($quantity)) {
+            foreach ($quantity as $qty) {
+                for ($i = 0; $i < $qty; $i++) {
+                    $sql = "INSERT INTO order_details (order_id, product_id, quantity) VALUES (:order_id, :product_id, 1)";
+                    $stmt = $this->conn->prepare($sql);
+                    $stmt->bindParam(':order_id', $orderID);
+                    $stmt->bindParam(':product_id', $productID);
+                    $stmt->execute();
+                }
+            }
+        } else {
+            for ($i = 0; $i < $quantity; $i++) {
+                $sql = "INSERT INTO order_details (order_id, product_id, quantity) VALUES (:order_id, :product_id, 1)";
+                $stmt = $this->conn->prepare($sql);
+                $stmt->bindParam(':order_id', $orderID);
+                $stmt->bindParam(':product_id', $productID);
+                $stmt->execute();
+            }
+        }
     }
 
-    public function clearBasket($customerID)
-    {
-        $sql = "DELETE FROM basket WHERE customer_id = :customer_id";
-        $stmt = $this->conn->prepare($sql);
-        $stmt->bindParam(':customer_id', $customerID);
-
-        return $stmt->execute();
-    }
-
-    public function lowerStockAmount($productID, $quantity)
+    private function lowerStockAmount($productID, $quantity)
     {
         $sql = "UPDATE products SET quantity = (quantity - :quantity) WHERE id = :product_id";
 
@@ -123,4 +109,42 @@ class FinishedPayment
             return false;
         }
     }
+
+    private function clearBasket($customerID)
+    {
+        $sql = "DELETE FROM basket WHERE customer_id = :customer_id";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindParam(':customer_id', $customerID);
+
+        return $stmt->execute();
+    }
+}
+
+class YourPaymentGateway implements PaymentGatewayInterface
+{
+    public function processPayment($customerID, &$errorMessage = "")
+    {
+        // Your actual payment gateway integration code here
+        // Return true for successful payment, false otherwise
+        // Also, handle the error message if necessary
+        // For example:
+        // $errorMessage = "Payment failed: Invalid credit card number."
+        return true;
+    }
+}
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $customerID = $_POST['customerID'];
+    $paymentManager = new PaymentManager($conn);
+    $paymentResult = $paymentManager->processPayment($customerID);
+
+    if ($paymentResult) {
+        echo "Payment successful! Basket cleared.";
+    } else {
+        http_response_code(500); // Internal Server Error
+        echo "Payment successful, but basket clearance failed.";
+    }
+} else {
+    http_response_code(405);
+    echo "Invalid request method.";
 }
